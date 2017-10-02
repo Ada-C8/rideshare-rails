ae095d56286d86d01475121bb773050dd399015a
Kayla Ecker <The Email Associated with Your Github Account>
2017-10-02 15:50:57 -0700

    Controllers/Models added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  get 'drivers/create'
+
+  get 'drivers/destroy'
+
+  get 'drivers/edit'
+
+  get 'drivers/index'
+
+  get 'drivers/new'
+
+  get 'drivers/show'
+
+  get 'drivers/update'
+
+  get 'passengers/create'
+
+  get 'passengers/destroy'
+
+  get 'passengers/edit'
+
+  get 'passengers/index'
+
+  get 'passengers/new'
+
+  get 'passengers/show'
+
+  get 'passengers/update'
+
+  get 'trips/create'
+
+  get 'trips/destroy'
+
+  get 'trips/edit'
+
+  get 'trips/index'
+
+  get 'trips/new'
+
+  get 'trips/show'
+
+  get 'trips/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
