0a2996e1db8a18df20e7cc3ed11344e2b4d4d7b2
Lauren <Laurenelee88@gmail.com>
2017-10-02 15:43:23 -0700

    set up passenger&driver tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/passengers", to: "passengers#index"
+  get "/drivers", to: "drivers#index"
 end
