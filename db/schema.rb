55d2ccb112b783ccf286a1df9552797e9253ac2e
Bennett Rahn <rahnibennett@gmail.com>
2017-10-02 15:36:58 -0700

    create trips, and give associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002222019) do
+ActiveRecord::Schema.define(version: 20171002223406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20171002222019) do
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "phone_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "driver_id"
+    t.integer "passenger_id"
+    t.date "date"
+    t.integer "rating"
+    t.integer "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
