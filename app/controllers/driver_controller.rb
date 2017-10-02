cef4b61bb661b7babf63e6d0efdb99aaa63a8757
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-02 16:08:28 -0700

    Display list of drivers on route /driver/index * Update routes to convention over configuration

---
+++ app/controllers/driver_controller.rb
@@ -0,0 +1,23 @@
+class DriverController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~App~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   root to: 'rideshare#index'
-  get '/rideshare', to: 'rideshare#index', as: 'rideshare_index'
-  get '/passengers', to: 'passenger#index', as: 'passenger_index'
+  get '/rideshare/index', to: 'rideshare#index', as: 'rideshare_index'
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~Passengers~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  get '/passenger/index', to: 'passenger#index', as: 'passenger_index'
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~Drivers~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  get '/driver/index', to: 'driver#index', as: 'driver_index'
+  get 'driver/show'
+
+  get 'driver/edit'
+
+  get 'driver/update'
+
+  get 'driver/new'
+
+  get 'driver/create'
+
+  get 'driver/destroy'
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~Trips~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 end
