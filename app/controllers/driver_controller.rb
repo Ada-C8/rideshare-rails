f90162d238c08856bf11a4b41450fa2acbf04ca4
Jessica Owens <vertige@gmail.com>
2017-10-02 16:36:55 -0700

    Created Driver Controller

---
+++ app/controllers/driver_controller.rb
@@ -0,0 +1,26 @@
+class DriverController < ApplicationController
+  #CRUD METHODS
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
