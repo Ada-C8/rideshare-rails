88231044e4e951ee0e2325c86925b4037c2beed1
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-02 14:46:46 -0700

    created controller and model for drivers

--- rideshare-rails/config/routes.rb
+++ rideshare-rails/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'drivers/index'
+
+  get 'drivers/show'
+
+  get 'drivers/edit'
+
+  get 'drivers/update'
+
+  get 'drivers/new'
+
+  get 'drivers/create'
+
+  get 'drivers/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
