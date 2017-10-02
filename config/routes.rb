4f0b7329c4ab6c76dc4c25e7460d2adc9b8e7e83
Rebecca Bergena <rbergena@gmail.com>
2017-10-02 16:45:54 -0700

    drivers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get "/drivers", to: "drivers#index", as: "drivers"
+  get "/drivers/:id", to: "drivers#show", as: "driver"
+  get "/drivers/new", to: "drivers#new", as: "new_driver"
+  get "drivers/:id/edit", to: "drivers#edit", as: "edit_driver"
+
+  post "/drivers",  to: "drivers#create", as: 'create_task'
+
+  patch "drivers/:id", to: 'drivers#update'
+
+
+  delete 'drivers/:id', to: 'drivers#destroy'
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
