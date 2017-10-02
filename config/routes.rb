9c155b7d018579bee22f1880ecacd69b9ce82259
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-02 14:39:12 -0700

    Added trips routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to "trips#index", as: "root"
+
+  get "/trips", to: "trips#index", as: "index"
+  get "trips/:id/edit", to: "trips#edit", as: "edit_trip"
+  get "trips/new", to: "trips#new", as: "new_trip"
+  get "trips/:id", to: "trips#show", as: "trip"
+  patch "trips/:id", to: "trips#update", as: "update_trip"
+  post "trips", to: "trips#create", as: "create_trip"
+  delete "trips/:id", to: "trips#destroy", as: "delete_trip"
+
 end
