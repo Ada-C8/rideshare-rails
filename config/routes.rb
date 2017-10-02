3c1b267712d2e25317e99058fc40dd72c06d0c50
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-02 15:01:50 -0700

    Added Passenger routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'passengers#index'
+
+  get '/passengers', to: 'passengers#index', as: 'passengers'
+
+  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
+
+  get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
+
+  get '/passengers/:id', to: 'passengers#show', as: 'passenger'
+
+  patch '/passengers/:id', to: 'passengers#update', as: 'update_passenger'
+
+  post '/passengers', to: 'passengers#create', as: 'create_passenger'
+
+  delete '/passengers/:id', to: 'passengers#destroy', as: 'delete_passenger'
+
 end
