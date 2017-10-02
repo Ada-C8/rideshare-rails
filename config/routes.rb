64e10b1f1a671d7307b8232456ef4a938a3822c4
hellomarisamorris <hellomarisamorris@gmail.com>
2017-10-02 16:46:48 -0700

    Created routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,56 @@
 Rails.application.routes.draw do
-  get 'trips/index'
+# resources :trips
+# resources :drivers
+# resources :passengers
 
-  get 'trips/show'
+  get '/trips/', to: 'trips#index', as: 'trips' #trips_path
 
-  get 'trips/create'
+  get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip' #edit_trips_path
 
-  get 'trips/update'
+  get '/trips/new', to: 'trips#new', as:'new_trip' #new_trip_path
 
-  get 'trips/destroy'
+  get '/trips/:id', to: 'trips#show', as:'trip' #trip_path
 
-  get 'trips/edit'
+  patch 'trips/:id', to: 'trips#update', as:'update_trip' #update_trip_path
 
-  get 'trips/new'
+  post '/trips', to: 'trips#create', as:'create_trip' #create_trip_path
 
-  get 'passengers/index'
+  delete '/trips/:id', to: 'trips#destroy', as: 'delete_trip' #delete_trip_path
 
-  get 'passengers/show'
 
-  get 'passengers/create'
 
-  get 'passengers/update'
 
-  get 'passengers/destroy'
+  get '/passengers/', to: 'passengers#index', as: 'passengers' #passengers_path
 
-  get 'passengers/edit'
+  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger' #edit_passengers_path
 
-  get 'passengers/new'
+  get '/passengers/new', to: 'passengers#new', as:'new_passenger' #new_passenger_path
 
-  get 'drivers/index'
+  get '/passengers/:id', to: 'passengers#show', as:'passenger' #passenger_path
 
-  get 'drivers/show'
+  patch 'passengers/:id', to: 'passengers#update', as:'update_passenger' #update_passenger_path
 
-  get 'drivers/create'
+  post '/passengers', to: 'passengers#create', as:'create_passenger' #create_passenger_path
 
-  get 'drivers/update'
+  delete '/passengers/:id', to: 'passengers#destroy', as: 'delete_passenger' #delete_passenger_path
 
-  get 'drivers/destroy'
 
-  get 'drivers/edit'
 
-  get 'drivers/new'
+
+  get '/passengers/', to: 'passengers#index', as: 'passengers' #passengers_path
+
+  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger' #edit_passengers_path
+
+  get '/passengers/new', to: 'passengers#new', as:'new_passenger' #new_passenger_path
+
+  get '/passengers/:id', to: 'passengers#show', as:'passenger' #passenger_path
+
+  patch 'passengers/:id', to: 'passengers#update', as:'update_passenger' #update_passenger_path
+
+  post '/passengers', to: 'passengers#create', as:'create_passenger' #create_passenger_path
+
+  delete '/passengers/:id', to: 'passengers#destroy', as: 'delete_passenger' #delete_passenger_path
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
