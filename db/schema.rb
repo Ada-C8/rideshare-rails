4a78639a78635cc33f0c37d0e0bfb08af415fd21
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-02 16:52:35 -0700

    Generated controller and model Passengers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002234226) do
+ActiveRecord::Schema.define(version: 20171002235036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20171002234226) do
   create_table "drivers", force: :cascade do |t|
     t.string "name"
     t.string "vin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
