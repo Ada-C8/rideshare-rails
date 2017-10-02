0447644d45e3fdc7f8452659bde3cdfdbb6215f4
Jessica Owens <vertige@gmail.com>
2017-10-02 16:34:23 -0700

    Created Rider Controller

---
+++ app/controllers/rider_controller.rb
@@ -0,0 +1,26 @@
+class RiderController < ApplicationController
+  #CRUD METHODS
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
