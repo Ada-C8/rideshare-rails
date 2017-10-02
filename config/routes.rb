0a164b383ef512b1528a03364f0637610f458043
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-02 16:02:06 -0700

    Add Passenger and Rideshare controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'rideshare#index'
+  get '/rideshare', to: 'rideshare#index', as: 'rideshare_index'
+  get '/passengers', to: 'passenger#index', as: 'passenger_index'
 end
