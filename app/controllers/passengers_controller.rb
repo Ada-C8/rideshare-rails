b0081fecb1ee31433fec2df6e206052cf6860fec
Tanisha <tanishahampden@gmail.com>
2017-10-03 12:31:07 -0700

    add new controller action

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,12 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    # @passenger = Passenger.new(passenger_)
+  end
 end
