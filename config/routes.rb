8f0db49dbf985b10cdc1321ee66ba81d1b0347a5
Bennett Rahn <rahnibennett@gmail.com>
2017-10-03 10:13:29 -0700

    add links to trip details

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :drivers
-resources :passengers
-resources :trips
-  get '/home', to:'home#index', as:'home'
+
+  root 'home#index'
+
+  resources :drivers
+  resources :passengers, only: [:index, :new, :create, :show] #no option to delete, edit, don't have extras lying around.
+  resources :trips
 end
