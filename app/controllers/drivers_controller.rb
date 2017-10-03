62b140af13e90d4c374acae29559357ad1d240ca
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-03 14:54:06 -0700

    Added create method to the drivers controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -17,9 +17,21 @@ class DriversController < ApplicationController
   end
 
   def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
   end
 
   def destroy
   end
 
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
