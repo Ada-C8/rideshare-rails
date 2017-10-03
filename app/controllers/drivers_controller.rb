7cd056f4ce58e003ff21c7facb1a83bb8b610ec9
IsabelDePapel <isuchanek@gmail.com>
2017-10-03 13:27:11 -0700

    Added new driver form and partial

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,17 +7,32 @@ class DriversController < ApplicationController
   end
 
   def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+
   end
 
   def edit
   end
 
   def new
+    @driver = Driver.new
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
   end
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,5 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  validates_presence_of :name, :vin
 end
