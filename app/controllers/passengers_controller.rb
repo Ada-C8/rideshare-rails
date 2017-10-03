2a2fa4c508f0ea4f6ba89ef2737a771e500dc19f
Sara C <srachandler@gmail.com>
2017-10-03 14:57:00 -0700

    added partial form for passenger new/edit

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -5,18 +5,30 @@ class PassengersController < ApplicationController
   end
 
   def new
+    @passenger = Passenger.new
   end
 
   def show
   end
 
   def edit
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+
+    unless @passenger
+      redirect_to passengers_path
+    end
   end
 
   def update
   end
 
   def create
+    @passenger = Passenger.new(name: params[:passenger][:name], phone_num: params[:passenger][:phone_num])
+    if @passenger.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
