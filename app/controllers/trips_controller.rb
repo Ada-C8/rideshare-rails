3d2c6fe1f2aefc1e554bb269ef1176e44cb9776c
Lauren <Laurenelee88@gmail.com>
2017-10-02 21:11:33 -0700

    add new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,27 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
   def show
-    @trip = Trip.find(params[id])
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(
+    driver_id: params[:trip][:driver_id],
+    passenger_id: params[:trip][:passenger_id],
+    date: params[:trip][:date],
+    cost: params[:trip][:cost],
+    rating: params[:trip][:rating]
+    )
+    if @trip.save
+    redirect_to trips_path
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+# new passenger, drivers, and trips:
   get "/passengers/new", to: "passengers#new"
   post "/passengers", to: "passengers#create"
   get "/drivers/new", to: "drivers#new"
   post "/drivers", to: "drivers#create"
-  
+  get "/trips/new", to: "trips#new"
+  post "/trips", to: "trips#create"
 
+# show all list
   get "/passengers", to: "passengers#index"
   get "/drivers", to: "drivers#index"
   get "/trips", to: "trips#index"
 
+# show individual details
   get "/passengers/:id", to: "passengers#show", as: "passenger"
   get "/drivers/:id", to: "drivers#show", as: "driver"
   get "/trips/:id", to: "trips#show", as: "trip"
