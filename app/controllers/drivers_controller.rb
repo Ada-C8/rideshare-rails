798c8ff94d960dccbf93ac28d23bb786202e484a
kee nam <nam.keeyeon@gmail.com>
2017-10-03 11:08:29 -0700

    Added view folders for drivers, passengers, and trips; also added all methods for drivers_controller.rb

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,42 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def new
+    @driver = Driver.new(driver_params)
+  end
+
+  def create
+    driver = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
+    driver.save
+    redirect_to drivers_path
+  end
+
+  def show
+    @driver = Driver.find(params[:id])
+  end
+
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    @driver = Driver.find(params[:id])
+    @driver.update_attributes(driver_params)
+    redirect_to driver_path(@driver)
+  end
+
+  def destroy
+    @driver = Driver.find(params[:id])
+    @driver.destroy
+    redirect_to drivers_path
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
+
 end
