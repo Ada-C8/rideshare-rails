ae975d9947c583c5a410fb893167e6822aa981e1
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-02 18:03:58 -0700

    added relationships in models

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'passengers/index'
-
-  get 'drivers/index'
-
-  get 'trips/index'
+   resources :drivers
+   resources :passengers
+   resources :trips
+   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
