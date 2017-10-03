11e0cb235227c90513efc19c5882ea7bc62b4f64
Eva Sawyer <esawyer25@gmail.com>
2017-10-02 21:06:19 -0700

    added Drivers#show and Drivers#index methods and layouts

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,6 +1,13 @@
 class DriversController < ApplicationController
-  def index
-  end
+  
+    def index
+      @drivers = Driver.all
+    end
+
+    def show
+      @driver = Driver.find(params[:id])
+    end
+
 
   #def show def new def create def edit def update def destroy
 end
