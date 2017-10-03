e88f2f94adb3754be5fca0e761430e2915705488
Eva Sawyer <esawyer25@gmail.com>
2017-10-02 21:04:58 -0700

    fixed a typo

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   get 'passengers/index'
 
-  "resources" :drivers
-  "resources" :passengers
-  "resources" :trips
+  resources :drivers
+  resources :passengers
+  resources :trips
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
