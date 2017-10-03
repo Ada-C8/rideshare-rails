d5865ee5c9de3ca9af2eb97d5355abc3030046f4
Severin Wiggenhorn <severin.randall@gmail.com>
2017-10-03 16:08:20 -0700

    added new driver functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,31 +3,31 @@ Rails.application.routes.draw do
 # trip routes
   root to: "trips#home", as: "root"
 
-  get "/trips", to: "trips#index", as: "trip_index"
+  get "/trips", to: "trips#index", as: "trips"
   get "trips/:id/edit", to: "trips#edit", as: "edit_trip"
   get "trips/new", to: "trips#new", as: "new_trip"
   get "trips/:id", to: "trips#show", as: "trip"
-  patch "trips/:id", to: "trips#update", as: "update_trip"
-  post "trips", to: "trips#create", as: "create_trip"
-  delete "trips/:id", to: "trips#destroy", as: "delete_trip"
+  patch "trips/:id", to: "trips#update"
+  post "trips", to: "trips#create"
+  delete "trips/:id", to: "trips#destroy"
 
 # driver routes
 
-get "/drivers", to: "drivers#index", as: "driver_index"
+get "/drivers", to: "drivers#index", as: "drivers"
 get "drivers/:id/edit", to: "drivers#edit", as: "edit_driver"
 get "drivers/new", to: "drivers#new", as: "new_driver"
 get "drivers/:id", to: "drivers#show", as: "driver"
-patch "drivers/:id", to: "drivers#update", as: "update_driver"
-post "drivers", to: "drivers#create", as: "create_driver"
-delete "drivers/:id", to: "drivers#destroy", as: "delete_driver"
+patch "drivers/:id", to: "drivers#update"
+post "drivers", to: "drivers#create"
+delete "drivers/:id", to: "drivers#destroy"
 
 # passenger routes
 
-get "/passengers", to: "passengers#index", as: "passenger_index"
+get "/passengers", to: "passengers#index", as: "passengers"
 get "passengers/:id/edit", to: "passengers#edit", as: "edit_passenger"
 get "passenger/new", to: "passenger#new", as: "new_passenger"
 get "passengers/:id", to: "passengers#show", as: "passenger"
-patch "passengers/:id", to: "passengers#update", as: "update_passenger"
-post "passengers", to: "passengers#create", as: "create_passenger"
-delete "passengers/:id", to: "passengers#destroy", as: "delete_passenger"
+patch "passengers/:id", to: "passengers#update"
+post "passengers", to: "passengers#create"
+delete "passengers/:id", to: "passengers#destroy"
 end
