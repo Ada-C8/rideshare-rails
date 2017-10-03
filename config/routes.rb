53f6fcc2e8258e30055a222b421e14f4f50e218e
VirtualLindsey <tapircode@gmail.com>
2017-10-02 18:55:58 -0700

    updates for the index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'drivers/index'
+  
   resources :trips
   resources :drivers
   resources :passengers
-  
+
+  root 'welcome#index'
+
 end
