8d15b5cb5db6f1af2e682b43d36584e6f333501c
murog <guillermina.muro@gmail.com>
2017-10-03 15:39:16 -0700

    updated routes to include resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,46 @@
 Rails.application.routes.draw do
-  get 'passengers/index'
-
-  get 'passengers/show'
-
-  get 'passengers/edit'
-
-  get 'passengers/update'
-
-  get 'passengers/new'
-
-  get 'passengers/create'
-
-  get 'passengers/destroy'
-
-  get 'drivers/index'
-
-  get 'drivers/show'
-
-  get 'drivers/edit'
-
-  get 'drivers/update'
-
-  get 'drivers/new'
-
-  get 'drivers/create'
-
-  get 'drivers/destroy'
-
-  get 'trips/index'
-
-  get 'trips/show'
-
-  get 'trips/edit'
-
-  get 'trips/update'
-
-  get 'trips/new'
-
-  get 'trips/create'
-
-  get 'trips/destroy'
+  resources :drivers, :passengers, :trips
+  # get 'passengers/index'
+  #
+  # get 'passengers/show'
+  #
+  # get 'passengers/edit'
+  #
+  # get 'passengers/update'
+  #
+  # get 'passengers/new'
+  #
+  # get 'passengers/create'
+  #
+  # get 'passengers/destroy'
+  #
+  # get 'drivers/index'
+  #
+  # get 'drivers/show'
+  #
+  # get 'drivers/edit'
+  #
+  # get 'drivers/update'
+  #
+  # get 'drivers/new'
+  #
+  # get 'drivers/create'
+  #
+  # get 'drivers/destroy'
+  #
+  # get 'trips/index'
+  #
+  # get 'trips/show'
+  #
+  # get 'trips/edit'
+  #
+  # get 'trips/update'
+  #
+  # get 'trips/new'
+  #
+  # get 'trips/create'
+  #
+  # get 'trips/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
