0bd7f9a4a8809f28404b2094ea4d42192d8e6956
Mariana <lmarianarp@gmail.com>
2017-10-03 10:01:15 -0700

    Home route and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   get 'trips/index'
 
   get 'passengers/index'
@@ -7,4 +9,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :drivers, :passengers, :trips
+  root 'home#index'
 end
