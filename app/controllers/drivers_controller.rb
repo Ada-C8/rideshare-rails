4e04dbeabc5cf6d520ab273fca5d934caeb521d5
Andrea Chen <andreachen.ac@gmail.com>
2017-10-02 20:41:45 -0700

    Add create and show actions and views to Driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,4 +7,18 @@ class DriversController < ApplicationController
   def new
     @driver = Driver.new
   end
+
+  def create
+    @driver = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @driver = Driver.find(params[:id])
+  end
+
 end
