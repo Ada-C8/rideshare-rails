b9f0e9cc26e23a80cc9250b201142a0eda4321e1
Bennett Rahn <rahnibennett@gmail.com>
2017-10-03 15:12:29 -0700

    add total_earned method for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -7,4 +7,15 @@ class Driver < ApplicationRecord
   # validates :name, uniqueness: { message: "This is your custom error message" }
   # you can add a uniqeness, format, length, numericality validation
   # each field gets a separate line, but each line can have multiple validations.
+
+  def total_earned
+    total = 0.0
+
+    trips.each do |trip|
+      total += trip.cost
+    end
+
+    total = ((total * 0.85) / 100).round(2)
+    #pretty sure this wont give .00 cents but its good enough for now
+  end
 end
