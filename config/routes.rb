c51ce486d5de27551a2f3e5b2b709ae62e0e68b8
Angela Wilson <wilsonange@gmail.com>
2017-10-03 09:16:44 -0700

    Nav Bar: Add links for 'All Passengers', 'All Drivers', 'Create Passengers', and 'Create Driver'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # show all list
+    get "/passengers", to: "passengers#index", as: "passengers"
+    get "/drivers", to: "drivers#index", as: "drivers"
+    get "/trips", to: "trips#index"
+
 # new passenger, drivers, and trips:
-  get "/passengers/new", to: "passengers#new"
+  get "/passengers/new", to: "passengers#new", as: "passengers_new"
   post "/passengers", to: "passengers#create"
-  get "/drivers/new", to: "drivers#new"
+  get "/drivers/new", to: "drivers#new", as: "drivers_new"
   post "/drivers", to: "drivers#create"
   get "/trips/new", to: "trips#new"
   post "/trips", to: "trips#create"
 
-# show all list
-  get "/passengers", to: "passengers#index"
-  get "/drivers", to: "drivers#index"
-  get "/trips", to: "trips#index"
+
 
 # show individual details
   get "/passengers/:id", to: "passengers#show", as: "passenger"
