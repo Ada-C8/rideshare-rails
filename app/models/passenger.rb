dc98439ba40ae86b255ebafe426089fee1025aea
Irene DeVera <irenneeyyy@gmail.com>
2017-10-03 14:28:13 -0700

    Add to passengers.index view and model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,13 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def calculate_total(id)
+    # @passenger = Passenger.find(params[:id])
+    all_trips = Trip.where(passenger_id: id)
+    sum = 0
+    all_trips.each do |trip|
+      sum += trip.cost
+    end
+    return sum
+  end
 end
