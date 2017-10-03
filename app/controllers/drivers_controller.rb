25ab065b8cec0222a8ab63b54d6724a21ba7c44f
Mariana <lmarianarp@gmail.com>
2017-10-03 12:33:22 -0700

    Chenge to controllers for drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,4 +2,29 @@ class DriversController < ApplicationController
   def index
     @drivers = Driver.all
   end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+  end
+
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    @driver = Driver.find(params[:id])
+    @driver.update_attributes(driver_params)
+    @driver.save
+    redirect_to driver_path(@driver)
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
