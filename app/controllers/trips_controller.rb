5721f77fe906434875b5ba1e9c497c0bceef5d85
sairagula <abdukhalieva@gmail.com>
2017-10-02 22:57:11 -0700

    trip_controller added

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,59 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(
+    driver_id: params[:trip][:driver_id],
+    passenger_id: params[:trip][:passenger_id],
+    date: params[:trip][:date].
+    rating: params[:trip][:rating],
+    cost: params[:trip][:cost]
+
+    )
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    result = @trip.update(
+    driver_id: params[:trip][:driver_id],
+    passenger_id: params[:trip][:passenger_id],
+    date: params[:trip][:date].
+    rating: params[:trip][:rating],
+    cost: params[:trip][:cost]
+    )
+    if result
+      redirect_to trip_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   trip = Trip.find(params[:id])
+  #   result = trip.destroy
+  #   if result
+  #     redirect_to trips_path
+  #   else
+  #     redirect_to trip_path(params[:id])
+  #   end
+  # end
 end
