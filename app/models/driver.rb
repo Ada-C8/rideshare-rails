c5a49f45a3954bbb59ea58d5cde6f129f5be9e29
Eva Sawyer <esawyer25@gmail.com>
2017-10-03 15:38:04 -0700

    Added average rating and total earnings logic

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,7 +1,23 @@
 class Driver < ApplicationRecord
   has_many :trip
 
-  validates :name, presence: {message: "A Driver must have a name."}
-  validates :vin, presence: {message: "A Driver must have a VIN number"}
+  validates :name, presence: {message: "Driver needs a name"}
+  validates :vin, presence: {message: "Driver needs a VIN number"}
 
+  def total_earned
+    total = 0
+    trip.each do |x|
+      total += x.cost
+    end
+    return total
+  end
+
+  def average_rating
+    total = 0.0
+    trip.each do |x|
+      total += x.rating
+    end
+    answer = total/trip.length
+    return answer.round(2)
+  end
 end
