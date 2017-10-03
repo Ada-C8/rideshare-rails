9e6ab9ada70d07e5c91021f6d43bc4c54578df6b
murog <guillermina.muro@gmail.com>
2017-10-03 16:20:48 -0700

    can create passenger, can view list of passengers with links to edit and delete

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,5 +1,6 @@
 class PassengersController < ApplicationController
   def index
+    @passengers = Passenger.order(:name)
   end
 
   def show
@@ -12,9 +13,17 @@ class PassengersController < ApplicationController
   end
 
   def new
+    @passenger = Passenger.new
   end
 
   def create
+    passenger = Passenger.new(name: params[:passenger][:name], phone_num: params[:passenger][:phone_num] )
+    if passenger.save
+      flash[:notice] = "#{passenger.name} was successfully saved"
+      redirect_to 'new_passenger_path'
+    else
+      render :new
+    end
   end
 
   def destroy
