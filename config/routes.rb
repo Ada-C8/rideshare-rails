025b04ea1d58dda17c19307d6d1a459380f9ef6d
Tanisha <tanishahampden@gmail.com>
2017-10-03 16:43:43 -0700

    add trips route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'trips/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :drivers, only: [:index, :new, :create, :show]
   resources :passengers, only: [:index, :new, :create, :show]
