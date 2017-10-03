c29ed65ddfd7cc8d615b0ef88684b6897f463693
Bennett Rahn <rahnibennett@gmail.com>
2017-10-03 15:53:47 -0700

    finish all attributes of passenger show page

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,14 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_spent
+    total = 0.0
+
+    trips.each do |trip|
+      total += trip.cost
+    end
+
+    total = (total / 100).round(2)
+  end
 end
