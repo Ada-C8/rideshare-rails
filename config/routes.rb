6bb7045b2ef33150837fe7b05abe2cd07daa5306
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-03 09:59:05 -0700

    Added main for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
    resources :drivers
    resources :passengers
    resources :trips
-   
+
+   root 'main#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
