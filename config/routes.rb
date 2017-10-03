ef40571d55c00f27f1ec815b83ee044a90ce5685
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-03 12:51:18 -0700

    Added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
-  
+
+  get 'passenger/index'
+
+  get 'passenger/new'
+
+  get 'passenger/show'
+
+  get 'passenger/edit'
+
+  get 'passenger/update'
+
+  get 'passenger/create'
+
+  get 'passenger/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homepage#index'
 
-  get '/passengers', to: 'passengers#index', as: 'passengers'
-
-  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
-
-  get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
-
-  get '/passengers/:id', to: 'passengers#show', as: 'passenger'
-
-  patch '/passengers/:id', to: 'passengers#update', as: 'update_passenger'
-
-  post '/passengers', to: 'passengers#create', as: 'create_passenger'
-
-  delete '/passengers/:id', to: 'passengers#destroy', as: 'delete_passenger'
+  resources :passengers
 
 end
