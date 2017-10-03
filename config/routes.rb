716c30a09455df2eb12eda1c391e6acfd24e8a4b
Shaunna <skwiens@gmail.com>
2017-10-03 13:29:40 -0700

    create new passenger feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,45 @@
 Rails.application.routes.draw do
-  get 'trips/index'
+  # get '/trips'
+  #
+  # get '/trips/show'
+  #
+  # get '/trips/new'
+  #
+  # get '/trips/create'
+  #
+  # get '/trips/edit'
+  #
+  # get '/trips/update'
+  #
+  # get '/trips/destroy'
 
-  get 'trips/show'
+  get '/passengers', to: 'passengers#index'
 
-  get 'trips/new'
+  get '/passengers/new', to: "passengers#new", as: "new_passenger"
 
-  get 'trips/create'
+  post '/passengers', to: "passengers#create"
 
-  get 'trips/edit'
+  # get '/passengers/show'
+  #
+  # get '/passengers/edit'
+  #
+  # get '/passengers/update'
+  #
+  # get '/passengers/destroy'
 
-  get 'trips/update'
+  get '/drivers', to: 'drivers#index'
 
-  get 'trips/destroy'
-
-  get '/passengers/index', to: 'passengers#index'
-
-  get 'passengers/show'
-
-  get 'passengers/edit'
-
-  get 'passengers/update'
-
-  get 'passengers/new'
-
-  get 'passengers/create'
-
-  get 'passengers/destroy'
-
-  get 'drivers/index', to: 'drivers#index'
-
-  get 'drivers/show'
-
-  get 'drivers/edit'
-
-  get 'drivers/update'
-
-  get 'drivers/new'
-
-  get 'drivers/create'
-
-  get 'drivers/destroy'
+  # get '/drivers/show'
+  #
+  # get '/drivers/edit'
+  #
+  # get '/drivers/update'
+  #
+  # get '/drivers/new', to: 'drivers#new'
+  #
+  # get '/drivers/create'
+  #
+  # get '/drivers/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
