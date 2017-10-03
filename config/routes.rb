12908472a24671542a65a256a6f2179888010483
Tanisha <tanishahampden@gmail.com>
2017-10-03 10:21:36 -0700

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  get 'trips/index'
-
-  get 'passengers/index'
-
-  get 'drivers/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :drivers, :passengers, :trips
+  resources :drivers, only: [:index, :new, :create]
+  resources :passengers, only: [:index, :new, :create]
+  resources :trips, only: [:index, :new, :create]
+
   root 'home#index'
 end
