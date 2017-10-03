beea4b88e8dc0010d118d9fc9958c4fbcc1d9559
Gale Harrington <gale.harrington19@gmail.com>
2017-10-02 17:57:07 -0700

    Add routes to drivers#index and passengers#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'passengers', to: 'passengers#index', as: 'passengers'
+
+  get 'drivers', to: 'drivers#index', as: 'drivers'
 end
