a738bd1b8fed11757600a19c688ecb655ca5adae
Laura Robertson <mslaura.mae@gmail.com>
2017-10-03 13:18:03 -0700

    Added edit and delete for Passenger

--- app/controllers/passenger_controller.rb
+++ app/controllers/passenger_controller.rb
@@ -14,17 +14,45 @@ class PassengerController < ApplicationController
   end
 
   def edit
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+
+    unless @passenger
+      redirect_to root_path
+    end
   end
 
   def update
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+    redirect_to passenger_path unless @passenger
+
+    if @passenger.update_attributes passenger_params
+      redirect_to passenger_path
+    else
+      render :edit
+    end
   end
 
   def new
+    @passenger = Passenger.new
   end
 
   def create
+    @passenger = Passenger.new passenger_params
+    if @passenger.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    Passenger.find_by(id: params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,32 @@ Rails.application.routes.draw do
   root to: 'rideshare#index'
   get '/rideshare/index', to: 'rideshare#index', as: 'rideshares'
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~Passengers~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  get '/passenger/index', to: 'passenger#index', as: 'passengers'
-  get 'passenger/:id', to: 'passenger#show', as: 'passenger'
-
-  get 'passenger/edit'
-
-  get 'passenger/update'
-
-  get 'passenger/new'
-
-  get 'passenger/create'
-
-  get 'passenger/destroy'
+  resources :passenger
+  # get '/passenger/index', to: 'passenger#index', as: 'passengers'
+  # get 'passenger/:id', to: 'passenger#show', as: 'passenger'
+  #
+  # get 'passenger/edit'
+  #
+  # get 'passenger/update'
+  #
+  # get 'passenger/new'
+  #
+  # get 'passenger/create'
+  #
+  # get 'passenger/destroy'
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~Drivers~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  get '/driver/index', to: 'driver#index', as: 'drivers'
-  get 'driver/:id', to: 'driver#show', as: 'driver'
-
-  get 'driver/edit'
-
-  get 'driver/update'
-
-  get 'driver/new'
-
-  get 'driver/create'
-
-  get 'driver/destroy'
+  resources :driver
+  # get '/driver/index', to: 'driver#index', as: 'drivers'
+  # get 'driver/:id', to: 'driver#show', as: 'driver'
+  #
+  # get 'driver/edit'
+  #
+  # get 'driver/update'
+  #
+  # get 'driver/new'
+  #
+  # get 'driver/create'
+  #
+  # get 'driver/destroy'
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~Trips~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 end
