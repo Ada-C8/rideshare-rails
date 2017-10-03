94135933300a39dfe427d2fd96cf1e348b4cf0c0
Amy Lee <amy.yj.lee@gmail.com>
2017-10-03 15:39:45 -0700

    Created partial form for editing and creating new drivers, created links to add new riders and drivers, created validations for creating new driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -15,7 +15,7 @@ class DriversController < ApplicationController
   end
 
   def new
-
+    @driver = Driver.new
   end
 
   def update
@@ -25,4 +25,7 @@ class DriversController < ApplicationController
   def destroy
 
   end
+
+private
+
 end
