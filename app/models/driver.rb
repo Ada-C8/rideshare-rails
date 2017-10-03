3935ac8b5dc2ad150312177f5bbb73af3cfcd59d
sairagula <abdukhalieva@gmail.com>
2017-10-03 15:03:21 -0700

    Calculated total earning and average rating of the driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,23 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earning
+    trips = Trip.where(driver_id: id)
+    sum = 0
+    trips.each do |trip|
+      sum += trip.cost
+    end
+    return sum
+  end
+
+  def average_rating
+    ratings = Trip.where(driver_id: id)
+    result = 0
+    counter = 0
+    ratings.each do |r|
+      result += r.rating
+      counter += 1
+    end
+    return (result/counter)
+  end
 end
