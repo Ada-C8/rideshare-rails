e7ca8e2fe3b200e668f144d16607478d584de57f
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-03 11:04:24 -0700

    update drivers#create and drivers#new to use strong params

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -9,7 +9,9 @@ class DriversController < ApplicationController
   end
 
   def create
-    @driver = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
+    # @driver = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
+    @driver = Driver.new(driver_params)
+
     if @driver.save
       redirect_to drivers_path
     else
@@ -27,26 +29,30 @@ class DriversController < ApplicationController
 
   def update
     @driver = Driver.find(params[:id])
-    result = @driver.update({
-      name: params[:driver][:name],
-      vin: params[:driver][:vin]
-      })
+    # result = @driver.update({
+    #   name: params[:driver][:name],
+    #   vin: params[:driver][:vin]
+    #   })
+    result = @driver.update(driver_params)
 
     if result
       redirect_to driver_path(@driver.id)
     else
       render :edit
     end
-
   end
 
   def destroy
     driver = Driver.find(params[:id])
     if driver.destroy
-    redirect_to drivers_path
+      redirect_to drivers_path
     else
-    #if unsuccessful show an error
+      #if unsuccessful show an error
     end
   end
 
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
