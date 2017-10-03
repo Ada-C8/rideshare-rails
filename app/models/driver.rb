70e9abb15255444a7f55f643cd67f4a87c417275
Lauren <Laurenelee88@gmail.com>
2017-10-03 11:54:53 -0700

    created validations for unique names for passengers and drivers and error messages

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,6 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: true, uniqueness: {message: "driver name is already in our system!"}
+  validates :vin, presence: true
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,6 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: true, uniqueness: {message: "passenger name is already in our system!"}
+  validates :phone_num, presence: true
 end
