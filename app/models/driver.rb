3cab7690305735ffe826d0471473b6cbfb9747bd
Andrea Chen <andreachen.ac@gmail.com>
2017-10-03 16:24:55 -0700

    Add validations to Driver model and create error_messages partial

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,9 +1,16 @@
 class Driver < ApplicationRecord
   has_many :trips
 
+  validates :name, presence: {message: "Please enter a name"}
+  validates :vin, presence: {message: "Please enter a VIN"}
+
   def average_rating
     avg = 0
     trips = Trip.where(driver_id: self.id)
+    if trips.length == 0
+      return 0
+    end
+    
     trips.each do |trip|
       avg += trip.rating
     end
