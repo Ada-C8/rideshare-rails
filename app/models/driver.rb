61bbfac7a02278f220ca7f6be45f04fa1443cee8
Gale Harrington <gale.harrington19@gmail.com>
2017-10-03 16:17:39 -0700

    Change driver model to respond to total_earnings

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,10 @@
 class Driver < ApplicationRecord
   has_many :trips
+  def total_earnings
+    total_earnings = 0
+    self.trips.each do |trip|
+      total_earnings += trip.cost
+    end
+    total_earnings *= 0.85
+  end
 end
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -4,4 +4,15 @@ class DriverTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # describe 'total_earnings' do
+  #   let(:trip_1) {Trip.new}
+  #   trip_1.driver.name = "Gale H"
+  #   let(:driver) {Driver.new(name: "Gale H", vin:"1")}
+  #   it 'Returns a number' do
+  #
+  #   end
+  #   it 'Sums the cost of a driver\'s trips and subtracts 15%' do
+  #
+  #   end
+  # end
 end
