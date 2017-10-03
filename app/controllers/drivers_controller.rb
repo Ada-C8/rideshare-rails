45bfca0f5b12fa65b8aca193cf9cf53410a62e29
Andrea Chen <andreachen.ac@gmail.com>
2017-10-02 20:55:14 -0700

    Add edit, update and destroy actions and views for Driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -21,4 +21,32 @@ class DriversController < ApplicationController
     @driver = Driver.find(params[:id])
   end
 
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    @driver = Driver.find(params[:id])
+    result = @driver.update({
+      name: params[:driver][:name],
+      vin: params[:driver][:vin]
+      })
+
+    if result
+      redirect_to driver_path(@driver.id)
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    driver = Driver.find(params[:id])
+    if driver.destroy
+    redirect_to drivers_path
+    else
+    #if unsuccessful show an error
+    end
+  end
+
 end
