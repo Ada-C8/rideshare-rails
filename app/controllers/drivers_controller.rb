928add55553e186d037ca60b830c4ac89fc25c6e
Eva Sawyer <esawyer25@gmail.com>
2017-10-02 21:48:15 -0700

    added a functioning Driver#destroy, and Driver#edit and Driver#new that won't update the database

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,11 +1,43 @@
 class DriversController < ApplicationController
-  
+
     def index
       @drivers = Driver.all
     end
 
     def show
       @driver = Driver.find(params[:id])
+    end
+
+    def new
+      @driver = Driver.new
+    end
+
+    def edit
+      @driver = Driver.find(params[:id])
+    end
+
+    def create
+     driver = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
+     driver.save
+     redirect_to drivers_path
+    end
+
+    def update
+    @driver = Driver.find(params[:id])
+    @driver.name = params[:driver][:name]
+    @driver.vin = params[:driver][:vin]
+    puts
+    puts @driver.name
+    @driver.save
+    puts
+    puts @driver.name
+    redirect_to drivers_path
+    end
+
+    def destroy
+    driver = Driver.find(params[:id])
+    driver.destroy
+    redirect_to drivers_path
     end
 
 
