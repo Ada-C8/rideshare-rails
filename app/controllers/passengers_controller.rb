874ac7949e0798ae1cd8fca4e9b4134f85c3ef06
Amy Cash <amymcash@gmail.com>
2017-10-02 19:09:31 -0700

    Add controller methods and view templates for passengers

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,11 +1,38 @@
 class PassengersController < ApplicationController
 
 def index
+  @passengers = Passenger.all
 end
 
+def show
+  @passenger = Passenger.find(params[:id])
+end
 
+def new
+  @passenger = Passenger.new
+end
 
+def create
+  @passenger = Page.new(params[:name, :phone_num])
+  @passenger.save
+  redirect_to @passenger
+end
 
+def edit
+  @passenger = Passenger.find(params[:id])
+end
+
+def update
+  @passenger = Page.find(params[:id])
+  @passenger.update(params[:name, :phone_num])
+  redirect_to @passenger
+end
+
+def destroy
+  @passenger = Passenger.find(params[:id])
+  @passenger.destroy
+  redirect_to pages_path
+end
 
 
 end
