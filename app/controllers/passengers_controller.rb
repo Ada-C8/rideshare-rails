bc2e14acfcd03a40f084f1b9b6c6c1344d43e692
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-03 15:13:22 -0700

    added validations to passenger and driver classes and handled error in the new driver view

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -17,8 +17,22 @@ class PassengersController < ApplicationController
   end
 
   def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
+
 end
