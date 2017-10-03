f40dfdccf1ef81eee2ffaa8a39c76bbe6e25ae7b
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-03 11:20:09 -0700

    add Driver#average_rating method to Driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,12 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def self.average_rating(id)
+    avg = 0
+    trips = Trip.where(driver_id: id)
+    trips.each do |trip|
+      avg += trip.rating
+    end
+    return avg / trips.count
+  end
 end
