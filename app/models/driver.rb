e1afb1bfd0e2611e41a0dedcc233fbb47e3620d3
Gale Harrington <gale.harrington19@gmail.com>
2017-10-03 16:33:01 -0700

    Add average_rating method to Driver model and display on driver#show page

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -7,4 +7,16 @@ class Driver < ApplicationRecord
     end
     total_earnings *= 0.85
   end
+  def average_rating
+    sum_ratings = 0
+    self.trips.each do |trip|
+      sum_ratings += trip.rating
+    end
+    if self.trips.length == 0
+      return 'No Ratings'
+    else
+      average = sum_ratings/(self.trips.length)
+      return average
+    end
+  end
 end
