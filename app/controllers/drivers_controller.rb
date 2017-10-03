2fb29c0119de8782c8899f5fa02a6a46ab4f77d8
Gale Harrington <gale.harrington19@gmail.com>
2017-10-03 13:07:14 -0700

    Use resources in routes.rb, create new and create actions and views for drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,4 +7,23 @@ end
 def show
   @driver = Driver.find( params[:id].to_i )
 end
+
+def new
+  @driver = Driver.new
+end
+
+def create
+  @driver = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
+  if @driver.save
+    redirect_to drivers_path
+  else
+    render :new
+  end
+end
+
+private
+
+def drivers_params
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  get 'passengers', to: 'passengers#index', as: 'passengers'
+  resources :drivers
+  # get 'drivers', to: 'drivers#index', as: 'drivers'
+  # get 'drivers/:id', to: 'drivers#show', as: 'driver'
 
-  get 'drivers', to: 'drivers#index', as: 'drivers'
+  resources :passengers
+  # get 'passengers', to: 'passengers#index', as: 'passengers'
+  # get 'passengers/:id', to: 'passengers#show', as: 'passenger'
 
-  get 'passengers/:id', to: 'passengers#show', as: 'passenger'
-
-  get 'drivers/:id', to: 'drivers#show', as: 'driver'
 
   get 'trips', to: 'trips#index', as: 'trips'
 
