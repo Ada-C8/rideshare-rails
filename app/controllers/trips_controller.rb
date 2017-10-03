957d3343388629d43d61ebb7b4322ecb5fb4eb2d
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-03 16:17:04 -0700

    wrote code for Trips#Controller and made Trips#Inex view page. Started Trips#Show page.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,25 @@
 class TripsController < ApplicationController
+
+def index
+  @trips = Trip.all
+end
+
+def show
+  @trip = Trip.find(params[:id])
+end
+
+def update
+  @trip = Trip.find(params[:id])
+  @trip = Trip.update(trip_params)
+  @trip.save
+
+  redirect_to trip_path(@trip)
+end
+
+private
+
+def trip_params #may need to update this later depending what we want to allow to be updated
+  return params.require(:trip).permit(:driver, :passenger)
+end
+
 end
