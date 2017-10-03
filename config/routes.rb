77ff92c70107173866fd743b0bab6a1061b38b31
Lauren <Laurenelee88@gmail.com>
2017-10-03 09:57:31 -0700

    modified and updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
-  # resources :passengers
-  # resources :drivers
-  # resources :trips
-  # show all list
-  get "/passengers", to: "passengers#index", as: "passengers"
-  get "/drivers", to: "drivers#index", as: "drivers"
-  get "/trips", to: "trips#index"
+  resources :passengers
+  resources :drivers
+  resources :trips
 
-  # new passenger, drivers, and trips:
-  get "/passengers/new", to: "passengers#new", as: "passengers_new"
-  post "/passengers", to: "passengers#create"
-  get "/drivers/new", to: "drivers#new", as: "drivers_new"
-  post "/drivers", to: "drivers#create"
-  get "/trips/new", to: "trips#new"
-  post "/trips", to: "trips#create"
-
-
-
-  # show individual details
-  get "/passengers/:id", to: "passengers#show", as: "passenger"
-  get "/drivers/:id", to: "drivers#show", as: "driver"
-  get "/trips/:id", to: "trips#show", as: "trip"
+  # get "/passengers", to: "passengers#index", as: "passengers"
+  # get "/passengers/new", to: "passengers#new", as: "passengers_new"
+  # post "/passengers", to: "passengers#create"
+  # get "/passengers/:id", to: "passengers#show", as: "passenger"
+  #
+  # get "/drivers", to: "drivers#index", as: "drivers"
+  # get "/drivers/new", to: "drivers#new", as: "drivers_new"
+  # post "/drivers", to: "drivers#create"
+  # get "/drivers/:id", to: "drivers#show", as: "driver"
+  #
+  # get "/trips", to: "trips#index"
+  # get "/trips/new", to: "trips#new"
+  # post "/trips", to: "trips#create"
+  # get "/trips/:id", to: "trips#show", as: "trip"
 end
