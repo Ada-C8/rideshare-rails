4397f23e1eacd00de460e46f67033be5b08bd005
Amy Lee <amy.yj.lee@gmail.com>
2017-10-02 17:48:34 -0700

    Phantom Driver and Rider Controller edits

---
+++ app/controllers/driver_controller.rb
@@ -0,0 +1,26 @@
+class DriverController < ApplicationController
+  #CRUD METHODS
+  def index
+    @drivers = Driver.order(:id)
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
