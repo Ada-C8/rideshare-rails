766d7c84c12ce93623f029bb0ff809086ab26c7c
Tanisha <tanishahampden@gmail.com>
2017-10-02 17:03:45 -0700

    create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+
+  get 'passengers/index'
+
+  get 'drivers/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :drivers, :passengers, :trips
 end
