1a5888ec3169dfe33c8f5d6c1c84129f22d6b928
kee nam <nam.keeyeon@gmail.com>
2017-10-04 15:03:53 -0700

    Improved routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :drivers, :passengers do
-    resources :trips, only: [:index, :new, :show]
+  resources :passengers do
+    resources :trips, only: [:index, :new ]
   end
-  resources :trips, only: [:edit, :destroy]
+  resources :drivers do
+    resources :trips, only: [:index]
+  end
+  resources :trips, only: [:show, :edit, :destroy]
 end
