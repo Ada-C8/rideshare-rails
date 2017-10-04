cd220d18dd542f61299157f039a2b0eefd8647f0
Tanisha <tanishahampden@gmail.com>
2017-10-04 15:00:23 -0700

    add route for update action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :drivers, only: [:index, :new, :create, :show]
-  resources :passengers, only: [:index, :new, :create, :show, :edit]
+  resources :passengers, only: [:index, :new, :create, :show, :edit, :update]
   resources :trips, only: [:index, :new, :create, :show]
 
   root 'home#index'
