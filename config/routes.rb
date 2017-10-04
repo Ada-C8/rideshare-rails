6cbad2171bb0be613f8d4f8a297c2c12ceddd8f8
Gale Harrington <gale.harrington19@gmail.com>
2017-10-04 16:45:41 -0700

    Partial view working for driver forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :drivers
   # get 'drivers', to: 'drivers#index', as: 'drivers'
   # get 'drivers/:id', to: 'drivers#show', as: 'driver'
