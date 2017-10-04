40dbaa0fdf1dcb79afaa86e0a45d0e1d582707ea
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-04 14:45:40 -0700

    Added CRUD for passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,16 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: {message: "Please enter a name"}
+  validates :phone_num, presence: {message: "Please enter a phone number"}
+
+  def total_spent
+    total = 0
+
+    trips.each do |trip|
+      total += trip.cost
+    end
+    return total
+  end
+
 end
