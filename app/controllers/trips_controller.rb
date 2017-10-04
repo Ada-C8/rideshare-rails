ad8225f2cacd7c2770cdda186d00807b80d4cbe6
Lauren <Laurenelee88@gmail.com>
2017-10-04 16:00:27 -0700

    ADD NEW TRIPgit add .git add .

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,27 @@
 class TripsController < ApplicationController
   def index
-    @trips = Trip.order(:id)
+    if params[:id]
+      @trips = Passenger.find(params[:id]).trips
+    else
+      @trips = Trip.order(:id)
+    end
+  end
+
+  def new
+    @trip = Trip.new
+    if params[:passenger_id]
+      @trip.passenger_id = params[:passenger_id]
+    end
+  end
+
+  def create
+    @trip = Trip.new(trips_params
+    )
+    if @trip.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   # def show
@@ -25,7 +46,7 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
     result = @trip.update(trips_params)
     if result
-       redirect_to trips_path(@trip.id)
+      redirect_to trips_path(@trip.id)
     else
       render :edit
     end
