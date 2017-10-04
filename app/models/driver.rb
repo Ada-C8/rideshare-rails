66d12dafc23dff0b71c4cd282d51fae994a15821
Jessica Owens <vertige@gmail.com>
2017-10-03 18:07:44 -0700

    Added Driver#avg_rating and showed on show.html

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,9 +5,15 @@ class Driver < ApplicationRecord
   validates :vin, presence: true
 
   COMMISSION = ".85".to_f
+
   def total_earnings
     total_charges= self.trips.map{|trip| trip.cost}.reduce(:+)
 
     return '%.02f' % (total_charges * COMMISSION)
+  end
+
+  def avg_rating
+    avg = self.trips.map { |trip| trip.rating}.reduce(:+).to_f / self.trips.count
+    return '%.01f' % avg
   end
 end
