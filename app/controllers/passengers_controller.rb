5db5a3f099b16958165ceeec2b44e542ae463e29
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-04 15:10:50 -0700

    made edit page available for passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -20,6 +20,10 @@ class PassengersController < ApplicationController
     end
   end
 
+  def edit
+    @passenger = Passenger.find(params[:id])
+  end
+  
   def update
     @passenger = Passenger.find(params[:id])
     @passenger = Passenger.update(passenger_params)
