be71cc4eea5179382d8719551dfb30c408c6f3ed
Amy Lee <amy.yj.lee@gmail.com>
2017-10-03 17:53:24 -0700

    Added driver#total_earnings method to Driver Model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,11 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  COMMISSION = ".85".to_f
+  def total_earnings
+    total_charges= self.trips.map{|trip| trip.cost}.reduce(:+)
+
+    return '%.02f' % (total_charges * COMMISSION)
+  end
 end
