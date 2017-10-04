1d658be0f3d893860b3b8eb2914d93ade34a661e
Sara C <srachandler@gmail.com>
2017-10-04 16:51:53 -0700

    updated trip links from passenger, adding logic to trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,15 +3,30 @@ class TripsController < ApplicationController
   end
 
   def new
+    @trip = Trip.new
   end
 
   def show
+    @trip = Trip.find(params[:id])
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id].to_i)
+
+    unless @trip
+      redirect_to passenger_trips_path
+    end
   end
 
   def update
+    trip = Trip.find_by(id: params[:id].to_i)
+    redirect to passenger_trips_path unless trip
+
+    if trip.update_attributes passenger_params
+      redirect_to passenger
+    else
+      render :edit
+    end
   end
 
   def create
