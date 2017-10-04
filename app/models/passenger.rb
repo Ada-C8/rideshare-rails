1375488dba4f02737dc90d6daf3e0c7434ee9f3d
enigmagnetic <lauren.cardella@gmail.com>
2017-10-03 17:29:12 -0700

    Add total_trip_cost method to passenger model and add total cost to passenger show view.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,8 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_trip_cost
+    trips = Trip.where(passenger_id: self.id).collect { |trip| trip.cost }
+    trips.inject { |sum, trip_cost| sum + trip_cost }
+  end
 end
