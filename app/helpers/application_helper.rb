69d7fb7e1a01f6472efd7254a5ad489d5762f43f
murog <guillermina.muro@gmail.com>
2017-10-04 16:56:31 -0700

    added helper method to format money correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def money_format(input)
+    input = input.to_s
+    if input[-1] == "0"
+      return "$#{input}0"
+    else
+      return "$#{input}"
+    end
+  end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,4 +5,13 @@ class Passenger < ApplicationRecord
   #   trips = (Trip.all_trips("passengers"))[input_id.to_i]
   #   return trips
   # end
+  def total_spent
+    trips = self.trips
+    sum = 0
+    trips.each do |trip|
+      sum += trip.cost
+    end
+    return sum
+  end
+
 end
