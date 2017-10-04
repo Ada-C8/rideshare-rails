fab8250df3afe0d575812f26f97b731b2e7493db
Lauren <Laurenelee88@gmail.com>
2017-10-03 18:59:31 -0700

    still buggy but working on new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,33 +3,56 @@ class TripsController < ApplicationController
     @trips = Trip.all
   end
 
+  # def show
+  #   @trip = Trip.find(params[:id])
+  # end
   def show
     @trip = Trip.find(params[:id])
+    unless @trip
+      redirect_to root_path
+    end
+    @passenger = @trip.passenger
+    @driver = @trip.driver
   end
 
+  # def new
+  #   @trip = Trip.new
+  # end
+
+  # def create
   def new
-    @trip = Trip.new
+    driver_ids = find_driver_ids()
+    @trip = Trip.new(driver_id: driver_ids.sample.to_i, passenger_id: params[:passenger_id], date: Date.today, cost: rand(100..2000), rating: nil)
+    if @trip.save
+      redirect_to passenger_path(params[:passenger_id])
+    else
+      redirect_to root_path
+    end
   end
-
-  def create
-    trip = Trip.new(
-    driver_id: Driver.first,
-    passenger_id: Passenger.find_by(params[:id]),
-    date: Date.today,
-    cost: rand(10..100),
-    rating: 0
-    )
-    trip.save!
-    # puts @trip.errors.inspect
-    # if trip.save
-    #   redirect_to trips_path
-    # else
-    #   render :new
-    # end
-  end
+  # trip = Trip.new(
+  # driver_id: Driver.first,
+  # passenger_id: Passenger.find_by(params[:id]),
+  # date: Date.today,
+  # cost: rand(10..100),
+  # rating: 0
+  # )
+  # trip.save!
+  # puts @trip.errors.inspect
+  # if trip.save
+  #   redirect_to trips_path
+  # else
+  #   render :new
+  # end
 
   private
-  def trips_params
-    return params.require(:trip).permit(:driver_id, :passenger_id, :date, :cost, :rating)
+  # def trips_params
+  #   return params.require(:trip).permit(:driver_id, :passenger_id, :date, :cost, :rating)
+  # end
+  def find_driver_ids
+    driver_ids = []
+    Driver.all.each do |driver|
+      driver_ids << driver.id
+    end
+    return driver_ids
   end
 end
