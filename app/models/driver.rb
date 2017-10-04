76a5ca578f84e5202c3e4568caa08d0bb5a5c851
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-10-03 17:01:11 -0700

    created passenger total cost. driver earnings in progress

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,13 @@ class Driver < ApplicationRecord
   has_many :trips, dependent: :destroy
   validates :name, presence: true
   validates :vin, presence: true
+
+  def total_earnings
+    total = 0
+    trips.each do |t|
+      total += t.cost
+    end
+    return total
+  end
+
 end
