fe976bdd23af979126cbbcde5cad7aba15bf09af
enigmagnetic <lauren.cardella@gmail.com>
2017-10-03 21:03:42 -0700

    Add edit view for trip, add edit, update, and destroy methods to trips controller.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,4 +6,30 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find_by(id: params[:id])
   end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    redirect_to edit_trip_path unless @trip
+
+    if @trip.update_attributes trip_params
+      redirect_to trips_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Trip.find_by(id: params[:id]).destroy
+
+    redirect_to trips_path
+  end
+
+  private
+    def trip_params
+      return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+    end
 end
