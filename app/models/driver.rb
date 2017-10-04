c7038790b45d3db9838b9e9761715eebf1baf365
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-04 13:28:31 -0700

    added to drivers#controller: method to get avg_rating; method to get total_earnings (minus 15%)

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,24 @@ class Driver < ApplicationRecord
 
   validates :name, presence: {message: "Driver must have a name."}
   validates :vin, presence: {message: "VIN must be entered."}
+
+  def total_earnings
+    sum = 0
+
+    driver.trips.each do |trip|
+      sum += trip.cost
+    end
+
+    return sum - (sum * 0.15)
+  end
+
+  def avg_rating
+    sum = 0
+    driver.trips.each do |trip|
+      sum += trip.rating
+    end
+
+    return (sum/driver.trips.count)
+    
+  end
 end
