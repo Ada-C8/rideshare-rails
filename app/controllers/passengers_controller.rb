97945404f6a2bd40cc84cbf129a2892703a74bd0
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-03 19:46:46 -0700

    fix trip details display page

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -13,8 +13,11 @@ class PassengersController < ApplicationController
 
   def create
     @passenger = Passenger.new(passenger_params)
-    @passenger.save
-    redirect_to passengers_path
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new
+    end
   end
 
   def update
