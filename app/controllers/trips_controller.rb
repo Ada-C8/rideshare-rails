e01e5ed124621a415527e4ea34435fb35036eb47
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-05 15:21:41 -0700

    Changed redirect_to to redirect_back for trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -27,7 +27,7 @@ class TripsController < ApplicationController
     result = @trip.update(trip_params)
 
     if result
-      redirect_to trip_path(@trip.id)
+      redirect_back(fallback_location: trip_path(@trip.id))
     else
       render :edit
     end
