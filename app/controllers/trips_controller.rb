34effbc5d17f1222a05e15715d3199ecb20c4eda
Jessica Owens <vertige@gmail.com>
2017-10-04 17:42:33 -0700

    Added Trip#create method with link_to from Riders show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -19,12 +19,15 @@ class TripsController < ApplicationController
     trip.update_attributes(trip_params) ? (redirect_to trip_path) : (render :edit)
   end
 
-  def new
-
-  end
+  # def new
+  #   @trip = Trip.new
+  # end
 
   def create
-
+    rider_passed_in = Rider.find_by(id: params[:id])
+    @trip = Trip.new(rider: rider_passed_in, driver: Driver.all.sample, cost: rand(100..15000), rating: 0, date: Date.today)
+    #later logic: driver would be next available & cost would be based on something
+    @trip.save ? (redirect_to trip_path(@trip.id)) : (redirect_to rider_path(params[:id]))
   end
 
   def destroy
