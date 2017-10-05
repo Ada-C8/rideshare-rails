f8009a14900ae771093d97ce5a4f7409bcbc32fd
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-04 21:01:56 -0700

    Add validation logic

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,6 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: { message: "You need to provide a name" }
+  validates :phone_num, presence: { message: "You need to provide a telephone number" }
 end
