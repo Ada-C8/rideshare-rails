61584e27e44899e10946d697ba91aec778ce2455
Lauren <Laurenelee88@gmail.com>
2017-10-05 14:40:32 -0700

    new error handling for trip rating

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,5 @@ class Trip < ApplicationRecord
 
   belongs_to :passenger, optional: true
 
+  validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 0, message: "Rating must be a number between 0-5." }
 end
