cf3b5b03424012b81391e009a64bb7ba9266c603
Gale Harrington <gale.harrington19@gmail.com>
2017-10-05 13:38:03 -0700

    Refactor driver.rb to DRY out methods

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,23 +3,17 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
-  
+
   def total_earnings
-    total_earnings = 0
-    self.trips.each do |trip|
-      total_earnings += trip.cost
-    end
+    total_earnings = trips.inject(0) { |sum, trip| sum + trip.cost }
     total_earnings *= 0.85
   end
   def average_rating
-    sum_ratings = 0
-    self.trips.each do |trip|
-      sum_ratings += trip.rating
-    end
-    if self.trips.length == 0
+    if trips.length == 0
       return 'No Ratings'
     else
-      average = sum_ratings/(self.trips.length)
+      sum_ratings = trips.inject(0) { |sum, trip| sum + trip.rating }
+      average = sum_ratings/(trips.length)
       return average
     end
   end
