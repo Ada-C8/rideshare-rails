94f4996cd1ed6d9636cb3ee8eb20d0928d01582f
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-05 14:50:53 -0700

    stylize hover over trip links

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,24 +10,19 @@ end
 
 def edit
   @trip = Trip.find(params[:id])
-  @driver = @trip.driver
 end
 
 def update
-  puts "does it work?????"
-  puts trip_params
   @trip = Trip.find(params[:id])
-  @trip.update_attributes(trip_params)
-  if @trip.save
-    redirect_to trip_path(@trip)
-  else
-    render :edit
-  end
+  @trip.update(trip_params)
+  @trip.save
+
+  redirect_to trip_path(@trip)
 end
 
 private
 
-def trip_params # may need to update this later depending what we want to allow to be updated
+def trip_params
   return params.require(:trip).permit(:rating, :cost)
 end
 
