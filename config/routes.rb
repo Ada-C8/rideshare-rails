7146ae690bea6b1a91519b03fe4930852bbe3efd
enigmagnetic <lauren.cardella@gmail.com>
2017-10-05 00:14:07 -0700

    Add new_passenger_trip path to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'passengers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   patch '/drivers/:id/available', to: 'drivers#available', as: 'driver_available'
   resources :drivers
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :passengers
   get 'passengers/:id/trips', to: 'trips#index', as: 'passenger_trips'
+  get 'passengers/:id/trips/new', to: 'trips#new', as: 'new_passenger_trip'
   # get 'passengers', to: 'passengers#index', as: 'passengers'
   # get 'passengers/:id', to: 'passengers#show', as: 'passenger'
 
