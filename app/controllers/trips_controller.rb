b8846a5bf5db0a1b09bb3e7c19a0c26f5802bae0
nkiru onwuneme <generalhrh@gmail.com>
2017-10-04 18:26:55 -0700

    Added list of trips to drivershow

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,15 @@
 class TripsController < ApplicationController
+  # def index
+  #   if params[:driver_id]
+  #     @trips = Driver.find(params[:driver_id]).trips.order(date: :desc)
+  #   else
+  #     @trips = Trip.all.order(date: :desc)
+  #   end
+  # end
+
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :drivers
+  resources :drivers do
+    # get '/trips', to: 'trips#index'
+  end
+
   resources :passengers
   resources :trips
 
