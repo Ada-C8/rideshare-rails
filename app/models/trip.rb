842328ae9c313d533142032e8f685fed402cd117
sairagula <abdukhalieva@gmail.com>
2017-10-05 14:53:23 -0700

    Updated random driver selection

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,7 +6,7 @@ class Trip < ApplicationRecord
   validates :rating, :cost, numericality: true
 
   def assign_driver
-    driver_id = rand(0..Driver.all.length)
+    driver_id = Driver.all.order("RANDOM()").first.id
     # Double check that we don't need to have the driver object or just the id of the driver
     # How to assign a driver that already has that date taken in the database
     return driver_id
