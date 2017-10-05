657957211d0ee609780afd238438ee6d781f09af
Irene DeVera <irenneeyyy@gmail.com>
2017-10-04 22:01:17 -0700

    Add styling to all pages

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,6 +4,8 @@ class Trip < ApplicationRecord
 
   def assign_driver
     driver_id = rand(0..Driver.all.length)
+    # Double check that we don't need to have the driver object or just the id of the driver
+    # How to assign a driver that already has that date taken in the database
     return driver_id
   end
 end
