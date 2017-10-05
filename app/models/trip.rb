dfa86a7c1e5967c75aaba3bf891ef1f96e8cc1f3
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-04 23:39:42 -0700

    Display error messages when editing trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,7 +6,7 @@ class Trip < ApplicationRecord
   validates :passenger_id, presence: {message: "Please select a passenger"}
   validates :date, presence: {message: "Please select a date"}
   validates :rating, presence: {message: "Please enter a rating"},
-                      numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, message: "Please enter a rating from 1-5"}
+                      numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, message: "Please enter a rating from 1-5 or 0 for no rating"}
   validates :cost, presence: {message: "Please enter the ride cost"},
                       numericality: {only_integer: true, message: "Ride cost should be in cents"}
 end
