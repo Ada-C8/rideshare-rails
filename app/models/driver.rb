9820cec6afcde4dfacb89cb1ad362110d90f8a3a
Rebecca Bergena <rbergena@gmail.com>
2017-10-05 16:20:30 -0700

    add money gem to gemfile, delete methods in views, create necessary model methods

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,4 @@
+
 class Driver < ApplicationRecord
   has_many :trips
 
@@ -9,5 +10,25 @@ class Driver < ApplicationRecord
 
   def self.select_driver
     return Driver.all.sample
+  end
+
+  def average_rating
+    sum = 0
+    counter = 0
+    trips.each do |trip|
+      sum += trip.rating
+      counter += 1
+    end
+    average = sprintf('%.2f', sum.to_f/counter)
+    return average
+  end
+
+  def total_earnings
+    sum = 0
+    trips.each do |trip|
+      sum += trip.cost
+    end
+    total_earnings = (0.85 * sum)
+    return Money.new(total_earnings, "USD").format
   end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,5 +4,12 @@ class Passenger < ApplicationRecord
   validates :name, presence: {message: "%{value} must be given"}
   validates :phone_num, presence: {message: "%{value} must be given"}
 
+  def charged
+    total_charged = 0
+    trips.each do |trip|
+      total_charged += trip.cost
+    end
+    return (Money.new(total_charged, "USD")).format
+  end
 
 end
