d3bc95b1f23de420ed198d7958c05ecfca2ea1d3
Tanisha <tanishahampden@gmail.com>
2017-10-05 16:32:41 -0700

    add average rating for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,22 @@ class Driver < ApplicationRecord
   validates :vin, presence:  {message: "Please enter a VIN" }
 
   has_many :trips
+
+  def total_amount
+    total = 0
+
+    trips.each do |trip|
+      total += trip.cost
+    end
+    return total
+  end
+
+  def average_rating
+    total = 0
+
+    trips.each do |trip|
+      total += trip.rating
+    end
+    return total / trips.length
+  end
 end
