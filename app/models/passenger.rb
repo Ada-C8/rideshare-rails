9e79caeb0e9ef1d94027f4a9497ea3f9605d0590
Lauren <Laurenelee88@gmail.com>
2017-10-05 16:00:03 -0700

    fix MAJOR BUG in trip detail page to link to passenger and driver

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,5 @@ class Passenger < ApplicationRecord
   has_many :trips
 
   validates :name, presence: true, uniqueness: {message: "passenger name is already in our system!"}
-  validates :phone_num, presence: true
+  validates :phone_num, presence: true, length:  {in: 7..10, message: "Phone number must be between 7-10 digits."}
 end
