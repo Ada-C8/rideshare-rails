067050dc9e23bead3f011874003895280dc54094
sairagula <abdukhalieva@gmail.com>
2017-10-06 11:19:18 -0700

    dependent_destroy added

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,5 @@
 class Driver < ApplicationRecord
-  has_many :trips
+  has_many :trips, dependent: :destroy
 
   validates :name, presence: true, format: {with: /[a-zA-Z]/}
   validates :vin, presence: true
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,5 @@
 class Passenger < ApplicationRecord
-  has_many :trips
+  has_many :trips, dependent: :destroy
 
   validates :name, presence: true, format: {with: /[a-zA-Z]/}
   validates :phone_num, presence: true
