73dffa7be0f69ee027a27333a85cf8ccdaacbf35
nkiru onwuneme <generalhrh@gmail.com>
2017-10-05 20:43:00 -0700

    Created total cost method for passenger in model, updated passenger show

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,12 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: { message: "You need to provide a name" }
   validates :phone_num, presence: { message: "You need to provide a telephone number" }
+
+  def total_cost
+    total_cost = 0
+    trips.each do |trip|
+      total_cost += trip.cost
+    end
+    return "$ #{total_cost/100.round(2)}"
+  end
 end
