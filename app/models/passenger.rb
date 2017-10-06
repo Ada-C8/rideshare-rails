c0df60a9196c574fe9e70e5463f86a78b200b4b0
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-06 11:10:21 -0700

    Model validations on Passenger and Driver, print errors for adding new passenger or driver

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,5 @@
 class Passenger < ApplicationRecord
   has_many :trips, dependent: :destroy
+  validates :name, presence: { message: 'Name must contain characters'}
+  validates :phone_num, presence: {message: 'Phone number must contain characters'}
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6, allow_nil: true, message: 'Rating must be an integer between 1 and 5'}
+  validates :cost, numericality: { only_integer: true, greater_than: 0, message: 'Cost must be a positive integer' }
 end
