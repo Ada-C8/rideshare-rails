6b2dce6e12df1110e06c95f1f518991711190493
Amy Lee <amy.yj.lee@gmail.com>
2017-10-06 15:18:04 -0700

    Major CSS styling

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -7,12 +7,9 @@ class Driver < ApplicationRecord
   COMMISSION = ".85".to_f
 
   def total_earnings
-    # if loop is for new drivers who don't have any trips, and therefore have no trip costs to add to earnings
-    #think about proper exception handling instead of returning nil
     if self.trips.count > 0
       total_charges= (self.trips.map{|trip| trip.cost}.reduce(:+))/100.0
 
-      #rounds up to nearest cent
       return '%.02f' % (total_charges * COMMISSION)
     else
       return nil
@@ -24,12 +21,6 @@ class Driver < ApplicationRecord
     avg = self.trips.map { |trip| trip.rating}.reduce(:+).to_f / count
     return '%.01f' % avg if avg > 0
   end
-
-  # def available?(date) #This is dummy logic for practice
-  #   self.trips.each do |trip|
-  #     trip.date == date ? (return false) : (return true)
-  #   end
-  # end
 
   def self.available #This is dummy logic for practice where driver can only have one rider per date
     self.joins(:trips).merge(Trip.having('MAX(date) < ?', Date.today).group(:id))
--- app/models/rider.rb
+++ app/models/rider.rb
@@ -5,7 +5,6 @@ class Rider < ApplicationRecord
   validates :phone, presence: true
 
   def total_cost
-    #if loop for new riders with no trips, think about proper exception handling instead of returning nil
     if self.trips.count > 0
       total = self.trips.map {|trip| trip.cost}.reduce(:+) / 100.0
       return '%.02f' % total
