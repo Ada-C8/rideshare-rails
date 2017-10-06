05b0629a36eb0d96c2ec7dc72e394b906492e8a7
Angela Wilson <wilsonange@gmail.com>
2017-10-06 15:19:08 -0700

    CSS for Passenger show page

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,5 +3,5 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true, uniqueness: {message: "passenger name is already in our system!"}
 
-  validates :phone_num, presence: true, length:  {in: 7..10, message: "Phone number must be between 7-10 digits."}
+  # validates :phone_num, presence: true, length:  {in: 7..10, message: "Phone number must be between 7-10 digits."}
 end
