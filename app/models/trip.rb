d8ace42c5f611d9139951b59dc0bef863f5aee7c
Angela Wilson <wilsonange@gmail.com>
2017-10-06 11:29:38 -0700

    resolving merge conflicts

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,12 +4,11 @@ class Trip < ApplicationRecord
   belongs_to :passenger, optional: true
 
   validates :rating, numericality: { only_integer: true, allow_nil: true, less_than_or_equal_to: 5, greater_than_or_equal_to: 0, message: "Rating must be a number between 0-5." }
-
-  # def self.total_cost(input)
-  #   total = @trip.cost / 100
-  #
-  #   total_cost += @trip.cost
-  # end
+  # use self because we are addressing behavior not attribute. The trip object is passed into the function and then the cost is returned.
+  def self.total_cost(input)
+      total = input.cost/100
+      return "%.2f"%total
+  end
 
   def self.grand_total(total_cost)
     final_total_cost = total_cost/100
@@ -18,4 +17,14 @@ class Trip < ApplicationRecord
   end
 
 
+    # def self.grand_total(total_cost)
+    #   final_total_cost = total_cost/100
+    #
+    #   return "%.2f"%final_total_cost
+    # end
+
+    def self.total_earning(trip)
+      total = ((trip.cost - (trip.cost * 0.15)) /100)
+       return "%.2f"%total
+    end
 end
