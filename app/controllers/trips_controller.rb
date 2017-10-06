e4a51a6b8a1c9615800ed0a0cb983315a684baa9
Amy Lee <amy.yj.lee@gmail.com>
2017-10-06 15:32:07 -0700

    Removed extraneous notes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,4 @@
 class TripsController < ApplicationController
-  def index
-    @trips = Trip.all
-  end
 
   def show
     @trip = Trip.find_by(id: params[:id].to_i)
@@ -19,20 +16,10 @@ class TripsController < ApplicationController
     trip.update_attributes(trip_params) ? (redirect_to trip_path) : (render :edit)
   end
 
-  def create
-    # #TODO: Refactor into Rider model method, so only Riders can create a new trip
-    # rider_passed_in = Rider.find_by(id: params[:id])
-    # @trip = rider_passed_in.start_trip
-    #
-    # @trip.save ? (redirect_to trip_path(@trip.id)) : (redirect_to rider_path(params[:id]))
-  end
-
   def destroy
-    # redirect_to trips_path if Trip.find_by(id: params[:id]).destroy
     if Trip.find_by(id: params[:id]).destroy
       flash[:notice] = "Trip deleted"
       redirect_to root_path
-      # redirect_back(fallback_location: root_path)
     end
   end
 
