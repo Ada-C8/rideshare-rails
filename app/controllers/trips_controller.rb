9d807e527438d7c0c30a975f791f2541cf1cfdfb
Isaac Del Rosario <cecdelr@gmail.com>
2017-10-06 11:58:09 -0700

    Created route logic for create's redirect, show links, and index links

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,7 +7,7 @@ class TripsController < ApplicationController
       @trips = Trip.where(driver_id: params[:driver_id])
       @trip_show_route = driver_trips_url
     else
-      @trips = Trip.all
+      @trips = Trip.order(:id)
       @trip_show_route = trips_url
     end
   end
@@ -19,10 +19,13 @@ class TripsController < ApplicationController
   def show
     if params[:passenger_id] != nil
       @trip = Trip.find_by(passenger_id: params[:passenger_id], id: params[:id])
+      @trip_show_route = passenger_trips_url
     elsif params[:driver_id] != nil
       @trip = Trip.find_by(driver_id: params[:driver_id], id: params[:id])
+      @trip_show_route = driver_trips_url
     else
       @trip = Trip.find(params[:id])
+      @trip_show_route = trips_url
     end
   end
 
@@ -38,7 +41,7 @@ class TripsController < ApplicationController
     trip = Trip.find_by(id: params[:id].to_i)
     redirect to passenger_trips_path unless trip
 
-    if trip.update_attributes passenger_params
+    if trip.update_attributes trip_params
       redirect_to passenger
     else
       render :edit
@@ -48,7 +51,7 @@ class TripsController < ApplicationController
   def create
     @trip = Trip.new(trip_params)
     if @trip.save
-      redirect_to @trip
+        redirect_to passenger_trip_path(id: @trip.id, passenger_id: params[:trip][:passenger_id])
     else
       render :new
     end
@@ -56,7 +59,13 @@ class TripsController < ApplicationController
 
   def destroy
     Trip.find_by(id: params[:id]).destroy
-    redirect_to passenger_path
+    if params[:passenger_id] != nil
+      redirect_to passenger_trips_url
+    elsif params[:driver_id] != nil
+      redirect_to driver_trips_url
+    else
+      redirect_to trips_url
+    end
   end
 
   private
