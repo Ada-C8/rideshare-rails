157330096dca3f8b9c8f81f8229b9e79bd45c728
enigmagnetic <lauren.cardella@gmail.com>
2017-10-05 23:57:18 -0700

    Add a StaticPages controller for Home and Help pages. Put PlaceCage images in list partial. Add header and footer to application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   root to: 'passengers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   patch '/drivers/:id/available', to: 'drivers#available', as: 'driver_available'
