e395c738620c2d471b177c7973a07b3538b4c39d
enigmagnetic <lauren.cardella@gmail.com>
2017-10-06 01:56:53 -0700

    Add default header and footer styles too application CSS. Add home and help page styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,19 @@
 Rails.application.routes.draw do
 
+root to: 'static_pages#home'
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  root to: 'passengers#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   patch '/drivers/:id/available', to: 'drivers#available', as: 'driver_available'
   resources :drivers do
     resources :trips, only: [:index]
   end
-  # get 'drivers', to: 'drivers#index', as: 'drivers'
-  # get 'drivers/:id', to: 'drivers#show', as: 'driver'
 
   resources :passengers do
     resources :trips, only: [:index, :new]
   end
-  # get 'passengers', to: 'passengers#index', as: 'passengers'
-  # get 'passengers/:id', to: 'passengers#show', as: 'passenger'
 
   resources :trips
 
-
-  #get 'trips', to: 'trips#index', as: 'trips'
-  #get 'trips/:id', to: 'trips#show', as: 'trip'
 end
