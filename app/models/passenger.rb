e06620228886e5e57e192f3457864fc50a65ce55
nkiru onwuneme <generalhrh@gmail.com>
2017-10-06 11:23:44 -0700

    Updated model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,8 +2,9 @@ class Passenger < ApplicationRecord
   # has_many :trips
   has_many :trips, -> { order(date: :desc) }
 
-  validates :name, presence: { message: "You need to provide a name" }
-  validates :phone_num, presence: { message: "You need to provide a telephone number" }
+  validates :date, presence: { message: "You need to provide a date" }
+  validates :rating, presence: { message: "You need to provide a rating" }
+  validates :cost, presence: { message: "You need to provide a cost" }
 
   def total_cost
     total_cost = 0
