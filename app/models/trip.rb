610fc672016c37045115071501aab55c516076e7
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-10-06 14:18:24 -0700

    Move validations for trip to trip model and create table in passengers_index

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
+  validates :date, presence: { message: "You need to provide a date" }
+  validates :rating, presence: { message: "You need to provide a rating" }
+  validates :cost, presence: { message: "You need to provide a cost" }
+
 end
