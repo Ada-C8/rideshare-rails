7c9f867e122023fde3cc62db47fb67bfc66b8166
Maria McGrew <mcgmar1@outlook.com>
2017-10-07 20:52:39 -0700

    added validation for negative trip cost

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,5 @@ class Trip < ApplicationRecord
   belongs_to :passenger
 
   validates_inclusion_of :rating, in: 1..5, :allow_nil => true
+  validates :cost, :numericality => { :greater_than_or_equal_to => 0 }
 end
