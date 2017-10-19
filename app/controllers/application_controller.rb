b9b914c43721d98232b8564e682a7fedb78a6d5c
Kimberley Zell <kimberley.zell@gmail.com>
2017-10-19 14:56:04 -0700

    added products controller index corrections and test corrections.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def save_and_flash(model)
+    result = model.save
+    if result
+      flash[:status] = :success
+      flash[:message] = "Sucessfully saved #{model.class} #{model.id}"
+    else
+      flash.now[:status] = :failure
+      flash.now[:message] = "failed to save #{model.class}"
+      flash.now[:details] = model.errors.messages
+    end
+    return result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
+   root 'main#index'
    resources :drivers
    resources :passengers
    resources :trips
 
-   root 'main#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
